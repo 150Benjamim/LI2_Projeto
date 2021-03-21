d77cc9f9f25342fa1464044cbb8a876971b1e37d
150Chakall <a93323@alunos.uminho.pt>
2021-03-21 19:51:26 +0000

    quase 40 porcento

--- Guiao1/parser.c
+++ Guiao1/parser.c
@@ -6,46 +6,55 @@
 #include "stack.h"
 
 
-void parse(char *line){
+void parse(char *line) {
 
     char *delims = " \t\n";
 
-    for (char *token = strtok (line,delims); token != NULL; token = strtok (NULL, delims)){
+    for (char *token = strtok(line, delims); token != NULL; token = strtok(NULL, delims)) {
         char *sobra;
-        long val_i  = strtol (token, &sobra, 10);
-        if(strlen(sobra)==0){
+        long val_i = strtol(token, &sobra, 10);
+        if (strlen(sobra) == 0) {
             PUSH(val_i);
-        }
-        else if(strcmp(token, "+")==0){
+        } else if (strcmp(token, "+") == 0) {
             long Y = POP();
             long X = POP();
-            PUSH (X+Y);
-        }
-        else if(strcmp(token, "-")==0){
+            PUSH(X + Y);
+        } else if (strcmp(token, "-") == 0) {
             long Y = POP();
             long X = POP();
-            PUSH (X-Y);
-        }
-        else if(strcmp(token, "*")==0){
+            PUSH(X - Y);
+        } else if (strcmp(token, "*") == 0) {
             long Y = POP();
             long X = POP();
-            PUSH (X*Y);
-        }
-        else if(strcmp(token, "/")==0){
+            PUSH(X * Y);
+        } else if (strcmp(token, "/") == 0) {
             long Y = POP();
             long X = POP();
-            PUSH (X/Y);
-        }
-        else if(strcmp(token, "(")==0){
+            PUSH(X / Y);
+        } else if (strcmp(token, "(") == 0) {
             long Y = POP();
-            PUSH (Y-1);
-        }
-        else if(strcmp(token, ")")==0){
+            PUSH(Y - 1);
+        } else if (strcmp(token, ")") == 0) {
             long Y = POP();
-            PUSH (Y+1);
+            PUSH(Y + 1);
+        } else if (strcmp(token, "%") == 0) {
+            long Y = POP();
+            long X = POP();
+            int x = X;
+            int y = Y;
+            int r = x % y;
+            PUSH (r);
+        } else if (strcmp(token, "#") == 0) {
+            long Y = POP();
+            long X = POP();
+            long x = X;
+            while (Y > 1) {
+                X *= x;
+                Y--;
+            }
+            PUSH(X);
         }
+
     }
-
     PRINT_STACK();
-
 }
