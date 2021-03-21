1280c99cf101e6e8492c5be818434fd24dffd559
Ferreira330 <73402966+Ferreira330@users.noreply.github.com>
2021-03-21 17:27:49 +0000

    Add files via upload

---
+++ eipl02g04-master/Guiao1/cmake-build-debug/CMakeFiles/guiao1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/benjamim/Desktop/projetoli2/Guioes/Guião1/main.c" "/home/benjamim/Desktop/projetoli2/Guioes/Guião1/cmake-build-debug/CMakeFiles/guiao1.dir/main.c.o"
+  "/home/benjamim/Desktop/projetoli2/Guioes/Guião1/parser.c" "/home/benjamim/Desktop/projetoli2/Guioes/Guião1/cmake-build-debug/CMakeFiles/guiao1.dir/parser.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
