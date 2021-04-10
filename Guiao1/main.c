dca00e3dc9e8af16f6c2b4e60e5f67c3dcb1aff8
150Chakall <a93323@alunos.uminho.pt>
2021-04-10 14:26:25 +0100

    sla

--- Guiao1/main.c
+++ Guiao1/main.c
@@ -3,6 +3,7 @@
 #include "stack.h"
 #include "parser.h"
 
+
 void imprime_tipo(STACK *s){
     if (has_type(top(s), STRING)) putchar ('S');
     if (has_type(top(s), INTEGER)) putchar ('I');
@@ -27,3 +28,18 @@ int main(void) {
 
     return 0;
 }
+
+/**
+int main() {
+
+    char line[10240];
+
+    assert ( fgets (line, 10240, stdin) != NULL);
+    assert ( line[strlen(line) - 1] == '\n'    );
+
+    parse(line);
+
+    return 0;
+}
+**/
+
