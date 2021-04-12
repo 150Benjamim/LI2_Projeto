cc5ead99e916af76b45348ab3c0c107731b83197
150Chakall <a93323@alunos.uminho.pt>
2021-04-12 12:00:16 +0100

    sla

--- Guiao1/parser.c
+++ Guiao1/parser.c
@@ -10,12 +10,16 @@ void parse(char *line) {
 
     char *delims = " \t\n";
 
+    char coca[10240];
+
+    char *fodasse;
+
     STACK *s = create_stack();
 
     for (char *token = strtok(line, delims); token != NULL; token = strtok(NULL, delims)) {
         char *sobra;
         long val_i = strtol(token, &sobra, 10);
-        if (strlen(sobra) == 0){
+        if (strlen(sobra) == 0) {
             push_LONG(s, val_i);
         } else if (strcmp(token, "+") == 0) {
             long Y = pop_LONG(s);
@@ -24,48 +28,57 @@ void parse(char *line) {
         } else if (strcmp(token, "-") == 0) {
             long Y = pop_LONG(s);
             long X = pop_LONG(s);
-            push_LONG(s,X - Y);
+            push_LONG(s, X - Y);
         } else if (strcmp(token, "*") == 0) {
             long Y = pop_LONG(s);
             long X = pop_LONG(s);
-            push_LONG(s,X * Y);
+            push_LONG(s, X * Y);
         } else if (strcmp(token, "/") == 0) {
             long Y = pop_LONG(s);
             long X = pop_LONG(s);
-            push_LONG(s,X / Y);
+            push_LONG(s, X / Y);
         } else if (strcmp(token, "(") == 0) {
             long X = pop_LONG(s);
-            push_LONG(s,X - 1);
+            push_LONG(s, X - 1);
         } else if (strcmp(token, ")") == 0) {
             long X = pop_LONG(s);
-            push_LONG(s,X + 1);
+            push_LONG(s, X + 1);
         } else if (strcmp(token, "%") == 0) {
             long Y = pop_LONG(s);
             long X = pop_LONG(s);
-            push_LONG(s,X % Y);
+            push_LONG(s, X % Y);
         } else if (strcmp(token, "#") == 0) {
             long Y = pop_LONG(s);
             long X = pop_LONG(s);
-            push_LONG(s,pow(X,Y));
+            push_LONG(s, pow(X, Y));
         } else if (strcmp(token, "&") == 0) {
             long Y = pop_LONG(s);
             long X = pop_LONG(s);
-            push_LONG(s,X & Y);
+            push_LONG(s, X & Y);
         } else if (strcmp(token, "|") == 0) {
             long Y = pop_LONG(s);
             long X = pop_LONG(s);
-            push_LONG(s,X | Y);
+            push_LONG(s, X | Y);
         } else if (strcmp(token, "^") == 0) {
             long Y = pop_LONG(s);
             long X = pop_LONG(s);
-            push_LONG(s,X ^ Y);
+            push_LONG(s, X ^ Y);
         } else if (strcmp(token, "~") == 0) {
             long X = pop_LONG(s);
-            push_LONG(s,~X);
+            push_LONG(s, ~X);
+        } else if (strcmp(token, "l") == 0) {
+            assert (fgets(coca, 10240, stdin) != NULL);
+            push_STRING(s, coca);
+        } else if (strcmp(token, "i") == 0) {
+            char *X = pop_STRING(s);
+            long Y = strtol(X, &sobra, 10);
+            push_LONG(s, Y);
+        } else if (strcmp(token, "f") == 0) {
+            char *X = pop_STRING(s);
+            double Y = strtod(X, &fodasse);
+            push_DOUBLE(s, Y);
         }
-
-        print_stack(s);
-
-        printf("\n");
     }
+
+    print_stack(s);
 }
