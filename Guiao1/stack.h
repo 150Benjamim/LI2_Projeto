c804f2d9de245c017343214a0f911c0d05f560c7
hugomes21 <a96842@alunos.uminho.pt>
2021-04-26 11:27:43 +0100

    Documentação do stack.h

--- Guiao1/stack.h
+++ Guiao1/stack.h
@@ -1,12 +1,29 @@
+/*!
+ * @file Ficheiro que contém as definições dos tipos.
+ */
+
 #ifndef GUIAO1_STACK_H
 #define GUIAO1_STACK_H
 
 #include <assert.h>
 
+/**
+ * @param line 14 - A função "enum" denomina os tipos com valores, de maneira a simplficar a sua chamada.
+ */
+
 typedef enum {LONG = 1, DOUBLE = 2, CHAR = 4, STRING = 8} TYPE;
+
+/**
+ * @param line 21 - Define que um tipo "LONG" e um tipo "CHAR" é um inteiro.
+ * @param line 22 - Define que um tipo "INTEGER" e um tipo "DOUBLE" é um número.
+ */
 
 #define INTEGER (LONG | CHAR)
 #define NUMBER (INTEGER | DOUBLE)
+
+/**
+ * @param line 28 - Apenas define os "data" correspondentes.
+ */
 
 typedef struct data {
     TYPE type;
@@ -15,6 +32,10 @@ typedef struct data {
     char CHAR;
     char *STRING;
 } DATA;
+
+/**
+ * @param line 40 - Indica que existe um inteiro para o tamanho e nº de elementos da stack.
+ */
 
 typedef struct stack {
     DATA *stack;
